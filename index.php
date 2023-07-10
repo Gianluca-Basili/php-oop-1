591d0e2ccf048afafd10256a36777f9a7dd5b9bf
Gianluca-Basili <gianluc.basili@gmail.com>
2023-07-10 14:01:46 +0200

    esercizio finito

--- index.php
+++ index.php
@@ -1,13 +1,52 @@
 <?php 
+
+    class Genere{
+        public $name;
+
+        public function __construct($name){
+            $this-> name = $name;
+
+        }
+    }
+    
     class Movie {
         public $title;
         public $year;
         public $genere;
         public $length;
         public $rating;
+        
 
-        public __constructor()
+        public function __construct($title, $year, Genere $genere, $length,){
+            $this-> title = $title;
+            $this-> year = $year;
+            $this-> genere = $genere;
+            $this-> length = $length;
+           
+
+
+
+        }
+
+        public function getMovieDetails(){
+            $string = "Movie:".$this->title.", Genere:".$this->genere->name.",year".$this -> year.",length:".$this -> length;
+
+            if($this -> rating !=null){
+                $string.= ",rating:".$this -> rating;
+            }
+
+            return $string;
+        }
     }
+
+    $v_per_vendetta = new Movie('V per vendetta', '2006', new Genere('azione'), 126);
+    $v_per_vendetta-> rating = 8;
+
+    $pulp_fiction = new Movie ('pulp fiction', '1994', new Genere('pulp'), 150 );
+
+    echo $v_per_vendetta->getMovieDetails();
+    echo "<br/>";
+    echo $pulp_fiction->getMovieDetails();
 ?>
 
 <!DOCTYPE html>
